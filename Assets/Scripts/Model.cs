695fe00a64b359bf27d00aed2419ee26646317bd
Shunya Ishikawa <91586590+shunyai@users.noreply.github.com>
2021-10-01 15:49:18 +0900

    Create Model.cs

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    private int[,] boardNow = new int[8, 6];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
