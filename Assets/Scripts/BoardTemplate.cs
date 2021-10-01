489cd886be9e9ecc966d0e2afa93f9789885f002
Shunya Ishikawa <91586590+shunyai@users.noreply.github.com>
2021-10-01 16:25:20 +0900

    Complete BoardTemplates.getNewBoard

--- Assets/Scripts/BoardTemplate.cs
+++ Assets/Scripts/BoardTemplate.cs
@@ -62,7 +62,7 @@ public class BoardTemplate : MonoBehaviour
         Random.InitState(System.DateTime.Now.Millisecond);
     }
 
-    public void getNewBoard()
+    public int[,] getNewBoard()
     {
         // Decide two boards randomly
         int boardNum1, boardNum2;
@@ -71,5 +71,26 @@ public class BoardTemplate : MonoBehaviour
         {
             boardNum2 = Random.Range(0, 10);
         }
+
+        // Merge the two boards
+        int[,] newBoard = new int[8, 6];
+        for(int y = 0; y < 4; y++)
+        {
+            for(int x = 0; x < 6; x++)
+            {
+                // Set a board upside down
+                newBoard[y, x] = this.boards[boardNum1, 3 - y, 5 - x];
+            }
+        }
+        for(int y = 4; y < 8; y++)
+        {
+            for(int x = 0; x < 6; x++)
+            {
+                // Set a board at the latter half of newBoard
+                newBoard[y, x] = this.boards[boardNum2, y + 3, x];
+            }
+        }
+
+        return newBoard;
     }
 }
